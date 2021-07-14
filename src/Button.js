7d4a8f0192ea11a52932297ec3e2454880923148
Losshi <64606103+Losshi@users.noreply.github.com>
2021-07-14 18:40:31 +0530

    Day 1 Assignment submitted

--- src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Button = ({actualText}) => {
-    return(
-        <button className="button"> {actualText} </button>
-    )
-}
-
-export default Button
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import GreetAll from "./GreetAll"
-import App from "./App"
-import "./style.css"
-
-ReactDOM.render(<GreetAll/>,document.getElementById("header"))
-ReactDOM.render(<App/>,document.getElementById("question"))
